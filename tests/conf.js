5012128f53557227c71200c5bc6caad38acc612d
jrbenny35 <bennyjr169@gmail.com>
2015-12-24 22:31:58 -0500

    Removed chrome as travis uses firefox

--- tests/conf.js
+++ tests/conf.js
@@ -2,11 +2,16 @@
 exports.config = {
     framework: 'jasmine2',
     specs: ['spec.js'],
+    capabilities: {
+        browserName: 'firefox'
+    },
+    /**
     multiCapabilities: [{
         browserName: 'firefox'
     }, {
         browserName: 'chrome'
     }],
+    **/
     onPrepare: function() {
         browser.driver.manage().window().maximize();
     }
